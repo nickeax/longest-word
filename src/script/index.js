b4f3ec874ea3dfedf907bcafb3f3f49c81da8636
Nick Fletcher <nickeax@gmail.com>
2020-06-11 17:32:17 +1000

    Refactor to use variable instead of file for words.

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,4 @@
+import { words } from './data.js';
 const MAX_LENGTH = 8;
 const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -5,10 +6,15 @@ const lett = document.querySelector('#lett');
 const inputLetters = document.querySelector('#inputLetters');
 const resultsHeading = document.querySelector('#resultsHeading');
 
+let accessedFlag = "goodbye";
 let results = [];
 
 inputLetters.addEventListener('keyup', _ => {
     lettersArr = [];
+    if (inputLetters.value.length > 9) {
+        inputLetters.value = "";
+    }
+
     buildInputLetters(inputLetters.value);
     fetchTest();
 })
@@ -32,24 +38,19 @@ function resetInputLetters() {
 }
 
 function buildInputLetters(str) {
-    for(let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
         lettersArr.push(new Letter(str[i]));
     }
     lett.innerHTML = "";
     lettersArr.forEach(x => {
         console.log(lettersArr);
-        
+
         lett.innerHTML += `<span class="inputLetters">${x.letter}</span>`;
     });
 }
 
-function processList(str) {
-    let arr = str.split('\n');
-    // arr = arr.filter(x => {
-    //     return x.length <= inputLetters.value.length;
-    // });
-
-    arr = arr.map(x => {
+function processList() {
+    let arr = words.map(x => {
         return x.trim();
     })
     return arr;
@@ -75,11 +76,9 @@ function longestWord(arr) {
 function canBeSpelt(str) {
     let numPassed = [];
     lettersArr.forEach(x => {
-        if(x.used != true) {
-            if(str.indexOf(x.letter) !== -1) {
-                x.used = true;
-                numPassed.push(1);
-            }
+        if (str.indexOf(x.letter) !== -1 && !x.used) {
+            x.used = true;
+            numPassed.push(1);
         }
     });
 
@@ -90,30 +89,17 @@ function canBeSpelt(str) {
 //     await fetchTest();
 // })();
 
-async function fetchTest() {
-    let response = await fetch('words.txt');
-    let responseText = await getTextFromStream(response.body);
-    let resArr = longestWord(processList(responseText));
-    
+function fetchTest() {
+    let resArr = longestWord(processList());
+    accessedFlag = "hello";
     let resultsList = document.querySelector('#resultsList');
-    resultsList.innerHTML = "";    
-    
+    resultsList.innerHTML = "";
+
     resArr.forEach(x => {
         resultsList.innerHTML += `<div class="resultsList">${x}</div>`;
     });
 }
 
 async function getTextFromStream(readableStream) {
-    let reader = readableStream.getReader();
-    let utf8Decoder = new TextDecoder();
-    let nextChunk;
-
-    let resultStr = '';
-
-    while (!(nextChunk = await reader.read()).done) {
-        let partialData = nextChunk.value;
-        resultStr += utf8Decoder.decode(partialData);
-    }
-
-    return resultStr;
+    return words;
 }
